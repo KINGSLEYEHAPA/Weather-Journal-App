ad361c1c8aaba1108f8af1ebb50130b5efd09acd
KINGSLEYEHAPA <kessity09@gmail.com>
2021-12-08 20:12:35 +0100

    Update of files for my weather app

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const { json } = require("body-parser");
 // const cors = require("cors");
 // app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000.");
+app.listen(5000, function () {
+  console.log("Server started on port 5000.");
 });
 app.use(express.static("website"));
-const projectData = [
-  {
-    temperature: 25,
-    date: "05 December 2021",
-    userResponse: "London",
-    temp: "file",
-  },
-];
+const projectData = { temp: "", date: "", userResponse: "", city: "" };
 
-app.get("/", (req, res) => {
-  res.send(projectData);
+app.get("/retrievedata", (req, res) => {
+  console.log(req.body);
+  const serverData = JSON.stringify(projectData);
+  res.send(serverData);
+  console.log(serverData);
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
-  projectData.push(req.body);
+app.post("/savedata", (req, res) => {
+  projectData.temp = req.body.temp;
+  projectData.date = req.body.date;
+  projectData.userResponse = req.body.userResponse;
+  projectData.city = req.body.city;
   console.log(projectData);
 
-  res.json("response:Data received");
+  res.json("Data received");
 });
--- website/app.js
+++ website/app.js
@@ -9,68 +9,106 @@ async function retrieveData(e) {
   const unit = "metric";
   const url = `https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=${apiKey}&units=${unit}&zip=${zip}`;
 
-  const newWeatherData = await getData(url);
-  const weatherCondition = document.querySelector(".condition-info");
-  const weatherConditionImage = document.querySelector(".condition img");
-  const location = document.querySelector(".city p");
-  const temp = document.querySelector(".temp-data");
+  const newWeatherData = await getData(url)
+    .then(function (newWeatherData) {
+      if (newWeatherData.cod === "404") {
+        const error = document.querySelector(".error");
+        error.textContent = newWeatherData.message;
+        setTimeout(function () {
+          error.textContent = "";
+        }, 3000);
+      }
+      const weatherConditionImage = document.querySelector(".condition img");
+      const weatherCondition = document.querySelector(".condition-info");
+      weatherCondition.textContent = newWeatherData.weather[0].description;
+      const icon = newWeatherData.weather[0].icon;
+      const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      weatherConditionImage.src = imageUrl;
+      const temp = newWeatherData.main.temp + "°C";
+      const city = newWeatherData.name;
+      const userResponse = document.querySelector("#user-feeling").value;
+      const date = new Date();
+      const days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+      let day = days[date.getDay()];
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      const month = months[date.getMonth()];
+      let dateNumber = date.getDate();
+      let year = date.getFullYear();
 
-  if (newWeatherData.cod === "404") {
-    const error = document.querySelector(".error");
-    error.textContent = newWeatherData.message;
+      const today = `${day} ${month} ${dateNumber}, ${year}.`;
+      console.log(today);
 
-    setTimeout(function () {
-      error.textContent = "";
-    }, 3000);
-  }
+      const data = { temp: "", date: "", userResponse: "", city: "" };
+      data.temp = temp;
+      data.date = today;
+      data.userResponse = userResponse;
+      data.city = city;
 
-  if (newWeatherData.main.temp > 25) {
-    temp.style.color = "red";
-  } else {
-    temp.style.color = "blue";
-  }
-  temp.textContent = newWeatherData.main.temp + "°C";
-
-  weatherCondition.textContent = newWeatherData.weather[0].description;
-  const icon = newWeatherData.weather[0].icon;
-  location.textContent = newWeatherData.name;
-  const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-  weatherConditionImage.src = imageUrl;
-
-  const date = new Date();
-  const days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
-  let day = days[date.getDay()];
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const month = months[date.getMonth()];
-  let dateNumber = date.getDate();
-  let year = date.getFullYear();
-
-  const today = `${day} ${month} ${dateNumber}, ${year}. `;
-  console.log(today);
-  const dateSpan = document.querySelector(".date");
-  dateSpan.textContent = today;
-  const userMessage = document.querySelector(".user-message");
-  const userInput = document.querySelector("#user-feeling").value;
-  userMessage.textContent = userInput;
-
-  const dashboard = document.querySelector(".dashboard");
-  dashboard.style.display = "flex";
-  document.querySelector("#cityInput").value = "";
-  document.querySelector("#user-feeling").value = "";
+      postData("/savedata", data);
+    })
+    .then(function () {
+      updateUI();
+    });
 }
+
+const updateUI = async () => {
+  const request = await fetch("/retrievedata");
+  try {
+    const serverData = await request.json();
+
+    const location = document.querySelector(".city p");
+    const temp = document.querySelector(".temp-data");
+
+    if (serverData.temp > 25) {
+      temp.style.color = "red";
+    } else {
+      temp.style.color = "blue";
+    }
+    temp.textContent = serverData.temp;
+
+    location.textContent = serverData.city;
+
+    const dateSpan = document.querySelector(".date");
+    dateSpan.textContent = serverData.date;
+    const userMessage = document.querySelector(".user-message");
+
+    userMessage.textContent = serverData.userResponse;
+
+    const dashboard = document.querySelector(".dashboard");
+    dashboard.style.display = "flex";
+    document.querySelector("#cityInput").value = "";
+    document.querySelector("#user-feeling").value = "";
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+//   const backgroundImage = document.querySelector("body").style.backgroundImage;
+//   const imageFile = [
+//     "url('img/broken-clouds.jfif')",
+//     "url('img/clear-sky.jpg')",
+//     "url('img/haze.jfif')",
+//     "url('img/heavy-rain.jpg')",
+//     "url('img/light-rain.jpg')",
+//     "url('img/light-snow.jfif')",
+//     "url('img/mist.jfif')",
+//     "url('img/overcast-clouds.jpg')",
+//     "url('img/snow.jpg')",
+//   ];
+
 const refresh = document.querySelector("#refresh");
 refresh.addEventListener("click", () => {
   dashboard.style.display = "none";
